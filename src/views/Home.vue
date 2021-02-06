14c94418b87482d161b79cfb790f6b3bf4a88e39
ysnslk <yasin.solak.26@gmail.com>
2021-02-06 23:27:09 +0300

    SideBar page created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+
+     <side-bar />
+
+    </div>
+
+
 </template>
 
 <script>
+import SideBar from '../components/Shared/SideBar.vue';
 export default {
+  components: { SideBar },
     name: "Home",
 };
 </script>
